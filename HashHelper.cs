058a955b61393eafb56edab7a4e00b23c04c9ac1
amacapozzi <amacapozzi07@gmail.com>
2024-10-01 14:56:15 -0300

    first commit

---
+++ HashHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ImageEncryptor
+{
+    internal class HashHelper
+    {
+        private static readonly string PublicKey = "8350e5a3";
+
+        private static readonly string PrivateKey = "d41d8cd9";
+
+        public static string Encrypt(string plainText)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(plainText)) return null;
+
+                string ToReturn = "";
+
+                byte[] secretkeyByte = { };
+                secretkeyByte = System.Text.Encoding.UTF8.GetBytes(PublicKey);
+                byte[] publickeybyte = { };
+                publickeybyte = System.Text.Encoding.UTF8.GetBytes(PrivateKey);
+                MemoryStream ms = null;
+                CryptoStream cs = null;
+                byte[] inputbyteArray = System.Text.Encoding.UTF8.GetBytes(plainText);
+                using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+                {
+                    ms = new MemoryStream();
+                    cs = new CryptoStream(ms, des.CreateEncryptor(publickeybyte, secretkeyByte),
+                                          CryptoStreamMode.Write);
+                    cs.Write(inputbyteArray, 0, inputbyteArray.Length);
+                    cs.FlushFinalBlock();
+                    ToReturn = Convert.ToBase64String(ms.ToArray());
+                }
+                return ToReturn;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+        }
+
+        public static string Decrypt(string encryptedText)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(encryptedText)) return null;
+
+                string ToReturn = "";
+
+                byte[] privatekeyByte = { };
+                privatekeyByte = System.Text.Encoding.UTF8.GetBytes(PublicKey);
+                byte[] publickeybyte = { };
+                publickeybyte = System.Text.Encoding.UTF8.GetBytes(PrivateKey);
+                MemoryStream ms = null;
+                CryptoStream cs = null;
+                byte[] inputbyteArray = new byte[encryptedText.Replace(" ", "+").Length];
+                inputbyteArray = Convert.FromBase64String(encryptedText.Replace(" ", "+"));
+                using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+                {
+                    ms = new MemoryStream();
+                    cs = new CryptoStream(ms, des.CreateDecryptor(publickeybyte, privatekeyByte),
+                                          CryptoStreamMode.Write);
+                    cs.Write(inputbyteArray, 0, inputbyteArray.Length);
+                    cs.FlushFinalBlock();
+                    Encoding encoding = Encoding.UTF8;
+                    ToReturn = encoding.GetString(ms.ToArray());
+                }
+                return ToReturn;
+            }
+            catch (Exception ae)
+            {
+                throw new Exception(ae.Message, ae.InnerException);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ImageEncryptor
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Select an option:");
+
+            Console.WriteLine("[+] 1. Encrypt Image\n[+] 2. Decrypt Image");
+
+            ConsoleKeyInfo key = Console.ReadKey(true);
+
+            while (true)
+            {
+                if (key.Key == ConsoleKey.D1)
+                {
+                    EncryptImageAsync();
+                }
+                else if (key.Key == ConsoleKey.D2)
+                {
+                    DecryptImageAsync();
+                }
+            }
+        }
+
+        private static void EncryptImageAsync()
+        {
+            try
+            {
+                Console.WriteLine("Please enter the image path to encrypt:");
+                string imagePath = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(imagePath) || !File.Exists(imagePath))
+                {
+                    Console.WriteLine("Invalid or non-existing image path");
+                    return;
+                }
+
+                Image image = Image.FromFile(imagePath);
+                byte[] imageBytes = ImageToByteArray(image);
+
+                string base64Image = Convert.ToBase64String(imageBytes);
+                string encryptedImage = HashHelper.Encrypt(base64Image);
+
+                Console.WriteLine("Encrypted Image Data:");
+                Console.WriteLine(encryptedImage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred: {ex.Message}");
+            }
+        }
+
+        public static void DecryptImageAsync()
+        {
+            try
+            {
+                Console.WriteLine("Please enter the encrypted image to decrypt:");
+                string encryptedImage = Console.ReadLine();
+
+                string decryptedBase64 = HashHelper.Decrypt(encryptedImage);
+                byte[] imgBytes = Convert.FromBase64String(decryptedBase64);
+
+                using (var ms = new MemoryStream(imgBytes))
+                {
+                    using (Bitmap bmp = new Bitmap(ms))
+                    {
+                        string outputFileName = "decrypted.png";
+                        bmp.Save(outputFileName);
+                        Console.WriteLine($"Image decrypted and saved as {outputFileName}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to decrypt image: {ex.Message}");
+            }
+        }
+
+        private static byte[] ImageToByteArray(Image image)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+    }
+}
